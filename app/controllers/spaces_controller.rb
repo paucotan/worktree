f3b587b4f9d4ac585ad10d2df36c93bf1ad938b8
paulcohentannugi <ctannugipaul@gmail.com>
2025-03-06 12:26:11 +0100

    Pato approves, user auth logic

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,5 @@
 class SpacesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @spaces = Space.all
